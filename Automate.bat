d3e91d7c7caab3b26d20d699a787cb57f9cf5248
mystic-cat <68018136+mystic-cat@users.noreply.github.com>
2020-09-29 10:01:07 -0700

    Added .bat to make running script easier

---
+++ Automate.bat
@@ -0,0 +1,2 @@
+"C:\Users\jared\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\jared\Documents\GitHub\Automatic-Udemy-Course-Enroller-GET-PAID-UDEMY-COURSES-for-FREE\udemy_enroller_chrome.py"
+pause
